bc34d2ae3a68501f697404aa0fd569b2816b617d
Sandhika-pramadhana <sandhikapramadana@gmail.com>
2024-11-16 16:16:58 +0700

    Second Commit

--- login.php
+++ login.php
@@ -10,41 +10,34 @@ if (isset($_COOKIE['user_id'])) {
 
 if (isset($_POST['submit'])) {
 
-   
+ 
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
     $pass = $_POST['pass'];
 
-    
+
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $message[] = 'Invalid email format!';
     } else {
         
-        
         if (isset($_SESSION['login_attempts']) && $_SESSION['login_attempts'] >= 5) {
-            
             if (isset($_SESSION['last_attempt_time']) && (time() - $_SESSION['last_attempt_time'] < 300)) {
-                $message[] = 'Too many failed login attempts.';
+                $message[] = 'Too many failed login attempts. Please try again after 5 minutes.';
             } else {
                 unset($_SESSION['login_attempts']);
                 unset($_SESSION['last_attempt_time']);
             }
         }
 
-        
         $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? LIMIT 1");
         $select_user->execute([$email]);
         $row = $select_user->fetch(PDO::FETCH_ASSOC);
 
-        
         if ($select_user->rowCount() > 0 && password_verify($pass, $row['password'])) {
             unset($_SESSION['login_attempts']);
             unset($_SESSION['last_attempt_time']);
-            
-            
             setcookie('user_id', $row['id'], time() + 60 * 60 * 24 * 30, '/', '', true, true);
             header('location:home.php');
         } else {
-            
             $_SESSION['login_attempts'] = isset($_SESSION['login_attempts']) ? $_SESSION['login_attempts'] + 1 : 1;
             $_SESSION['last_attempt_time'] = time();
             $message[] = 'Incorrect email or password!';
--- register.php
+++ register.php
@@ -2,123 +2,101 @@
 
 include 'components/connect.php';
 
-if (isset($_COOKIE['user_id'])) {
-    $user_id = $_COOKIE['user_id'];
-} else {
-    $user_id = '';
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
 }
 
-if (isset($_POST['submit'])) {
+if(isset($_POST['submit'])){
 
-    $id = create_unique_id();
+   $id = create_unique_id();
+   $name = $_POST['name'];
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $pass = sha1($_POST['pass']);
+   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+   $cpass = sha1($_POST['cpass']);
+   $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
 
-    
-    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $message[] = 'Invalid email format!';
-    }
-    
-    $pass = $_POST['pass'];
-    $cpass = $_POST['cpass'];
-    $hashed_pass = password_hash($pass, PASSWORD_DEFAULT);
+   $image = $_FILES['image']['name'];
+   $image = filter_var($image, FILTER_SANITIZE_STRING);
+   $ext = pathinfo($image, PATHINFO_EXTENSION);
+   $rename = create_unique_id().'.'.$ext;
+   $image_size = $_FILES['image']['size'];
+   $image_tmp_name = $_FILES['image']['tmp_name'];
+   $image_folder = 'uploaded_files/'.$rename;
 
-    
-    if ($pass != $cpass) {
-        $message[] = 'Confirm password does not match!';
-    } else {
-
-        $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
-        $select_user->execute([$email]);
-
-        if ($select_user->rowCount() > 0) {
-            $message[] = 'Email already taken!';
-        } else {
-
-            
-            if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
-                $image = $_FILES['image']['name'];
-                $ext = pathinfo($image, PATHINFO_EXTENSION);
-                $allowed_ext = ['jpg', 'jpeg', 'png', 'gif']; 
-                if (!in_array(strtolower($ext), $allowed_ext)) {
-                    $message[] = 'Invalid image format! Only JPG, JPEG, PNG, GIF are allowed.';
-                } else {
-                    $rename = create_unique_id() . '.' . $ext;
-                    $image_tmp_name = $_FILES['image']['tmp_name'];
-                    $image_folder = 'uploaded_files/' . $rename;
-
-                    move_uploaded_file($image_tmp_name, $image_folder);
-                }
-            } else {
-                $message[] = 'Profile picture is required!';
-            }
- 
-            if (empty($message)) {
-                $insert_user = $conn->prepare("INSERT INTO `users`(id, name, email, password, image) VALUES(?,?,?,?,?)");
-                $insert_user->execute([$id, $name, $email, $hashed_pass, $rename]);
-
-                
-                $verify_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? LIMIT 1");
-                $verify_user->execute([$email]);
-                $row = $verify_user->fetch(PDO::FETCH_ASSOC);
-
-                if ($verify_user->rowCount() > 0) {
-                    setcookie('user_id', $row['id'], time() + 60 * 60 * 24 * 30, '/');
-                    header('location:home.php');
-                }
-            }
-        }
-    }
+   $select_user = $conn->prepare("SELECT * FROM users WHERE email = ?");
+   $select_user->execute([$email]);
+   
+   if($select_user->rowCount() > 0){
+      $message[] = 'email already taken!';
+   }else{
+      if($pass != $cpass){
+         $message[] = 'confirm passowrd not matched!';
+      }else{
+         $insert_user = $conn->prepare("INSERT INTO users(id, name, email, password, image) VALUES(?,?,?,?,?)");
+         $insert_user->execute([$id, $name, $email, $cpass, $rename]);
+         move_uploaded_file($image_tmp_name, $image_folder);
+         
+         $verify_user = $conn->prepare("SELECT * FROM users WHERE email = ? AND password = ? LIMIT 1");
+         $verify_user->execute([$email, $pass]);
+         $row = $verify_user->fetch(PDO::FETCH_ASSOC);
+         
+         if($verify_user->rowCount() > 0){
+            setcookie('user_id', $row['id'], time() + 60*60*24*30, '/');
+            header('location:home.php');
+         }
+      }
+   }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Account</title>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Home</title>
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+   <link rel="stylesheet" href="css/style.css">
 </head>
-
 <body>
 
-    <?php include 'components/user_header.php'; ?>
+<?php include 'components/user_header.php'; ?>
 
-    <section class="form-container">
+<section class="form-container">
 
-        <form class="register" action="" method="post" enctype="multipart/form-data">
-            <h3>Create Account</h3>
-            <div class="flex">
-                <div class="col">
-                    <p>Your name <span>*</span></p>
-                    <input type="text" name="name" placeholder="Enter your name" maxlength="50" required class="box">
-                    <p>Your email <span>*</span></p>
-                    <input type="email" name="email" placeholder="Enter your email" maxlength="50" required class="box">
-                </div>
-                <div class="col">
-                    <p>Your password <span>*</span></p>
-                    <input type="password" name="pass" placeholder="Enter your password" maxlength="20" required class="box">
-                    <p>Confirm password <span>*</span></p>
-                    <input type="password" name="cpass" placeholder="Confirm your password" maxlength="20" required class="box">
-                </div>
-            </div>
-            <p>Select Profile Picture <span>*</span></p>
-            <input type="file" name="image" accept="image/*" required class="box">
-            <p class="link">Already have an account? <a href="login.php">Login Now</a></p>
-            <input type="submit" name="submit" value="Register Now" class="btn">
-        </form>
+   <form class="register" action="" method="post" enctype="multipart/form-data">
+      <h3>Create Account</h3>
+      <div class="flex">
+         <div class="col">
+            <p>Your name <span>*</span></p>
+            <input type="text" name="name" placeholder="Enter your name" maxlength="50" required class="box">
+            <p>Your email <span>*</span></p>
+            <input type="email" name="email" placeholder="Enter your email" maxlength="20" required class="box">
+         </div>
+         <div class="col">
+            <p>Your password <span>*</span></p>
+            <input type="password" name="pass" placeholder="Enter your password" maxlength="20" required class="box">
+            <p>Confirm password <span>*</span></p>
+            <input type="password" name="cpass" placeholder="Confirm your password" maxlength="20" required class="box">
+         </div>
+      </div>
+      <p>Select Profile Picture<span>*</span></p>
+      <input type="file" name="image" accept="image/*" required class="box">
+      <p class="link">Already have an account? <a href="login.php">Login Now</a></p>
+      <input type="submit" name="submit" value="register now" class="btn">
+   </form>
+</section>
 
-    </section>
+<?php include 'components/footer.php'; ?>
 
-    <?php include 'components/footer.php'; ?>
-
-    <script src="js/script.js"></script>
-
+<script src="js/script.js"></script>
+   
 </body>
-
 </html>
